074061fbee170c69da4da567be147ad26c4cd497
Ayca Gurler <berfingurler@hotmail.com>
2024-02-02 18:42:07 +0300

    first commit--Navbar and Hero

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Navbar from "./Components/Navbar"
+import Hero from "./Components/Hero"
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+        </div>
+    )
+}
---
+++ Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav className="nav">
+                <img src="../images/airbnb-logo.png" className="nav--icon"/>
+        </nav>
+    )
+}
