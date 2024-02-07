62285453a311ffe2e90c143d760e87ab1485c08e
Ayca Gurler <berfingurler@hotmail.com>
2024-02-07 18:25:03 +0300

    Adding more cards and badge

--- App.js
+++ App.js
@@ -2,22 +2,33 @@ import React from "react"
 import Navbar from "./Components/Navbar"
 import Hero from "./Components/Hero"
 import Card from "./Components/Card"
-
+import data from "./data"
 
 
 export default function App() {
+    // bu sekilde datayi direkt olarak aktarabiliriz
+    const cards = data.map(item => {
+        return(
+            <Card 
+                key= {item.id}
+                img= {item.coverImg}
+                rating= {item.stats.rating}
+                reviewCount= {item.stats.reviewCount}
+                country= {item.location}
+                title= {item.title}
+                price= {item.price}
+                openSpots= {item.openSpots}
+            />
+        )
+    })
+
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card 
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country= "Turkey"
-                title="Life Lessons with Katie Zaferes"
-                price= {136}
-            />
+            <section className="card-list">
+                {cards}
+            </section>
         </div>
     )
 }
--- Components/Card.js
+++ Components/Card.js
@@ -2,18 +2,28 @@ import React from "react"
 
 
 export default function Card(props) {
+
+    let badgeText
+    if(props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }else if(props.openSpots === "Online"){
+        badgeText = "ONLINE"
+    }
+
     return (
         <div className="card">
-        {/* eger img eklerken pathi degil de sadece png ismi olarak ekliyorsak asagidaki gibi ekleyebiliriz */}
-        <img src={`../images/${props.img}`} className="card--image" />
-        <div className="card--stats">
-            <img src="../images/star.png" className="card--star" />
-            <span>{props.rating}</span>
-            <span className="gray">({props.reviewCount}) • </span>
-            <span className="gray">{props.country}</span>
+            {/* bu sekilde yazdigimizda varsa ekliyor yoksa hic girmiyor -- conditional rendering*/}
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            {/* eger img eklerken pathi degil de sadece png ismi olarak ekliyorsak asagidaki gibi ekleyebiliriz */}
+            <img src={`../images/${props.img}`} className="card--image" />
+            <div className="card--stats">
+                <img src="../images/star.png" className="card--star" />
+                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
+            </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
-        <p>{props.title}</p>
-        <p><span className="bold">From ${props.price}</span> / person</p>
-    </div>
     )
 }
