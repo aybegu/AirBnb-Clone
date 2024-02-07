5a8dec1b7f49aa8ca6caf7b775b967931cc89d84
Ayca Gurler <berfingurler@hotmail.com>
2024-02-07 19:13:41 +0300

    Spread objects as props

--- App.js
+++ App.js
@@ -11,7 +11,9 @@ export default function App() {
         return(
             <Card 
                 key= {item.id} //map kullandigimiz her seferde key kullanmaliyiz
-                item={item}
+                /* bu sekilde yazmak yerine spread syntax ile yazabilriz,  butun itemleri kaldirmamiz gerekir
+                item={item} */
+                {...item}
             />
         )
     })
